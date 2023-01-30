13e4b5f2a08a739d40810368ef2bce7549da78df
LordBohne2 <work.qj684@aleeas.com>
2023-01-30 22:24:11 +0100

    Fix Falling Jump

--- lua/autorun/sh_skywalk.lua
+++ lua/autorun/sh_skywalk.lua
@@ -2,12 +2,27 @@ print("Skywalk...")
 
 hook.Add("KeyPress", "MidAirJump", function(ply, key)
     if key == IN_JUMP and ply:OnGround() == false then
-        if key == IN_SPEED then
+        if true then -- Direction Jump
             local angle = ply:EyeAngles()
             local direction = angle:Forward() * 250
-            ply:SetVelocity(ply:GetVelocity() + Vector(direction.x, direction.y, 250))
-        else
-            ply:SetVelocity(Vector(0, 0, 300))
+            print(angle)
+            if angle.x < -10 then -- Go Up
+                ply:SetVelocity(ply:GetVelocity() + Vector(direction.x, direction.y, direction.z + 300))
+                print("Down")
+            else -- Go Down
+                ply:SetVelocity(ply:GetVelocity() + Vector(direction.x, direction.y, direction.z))
+                print("Up")
+            end
+            print("Direction Jump")
+
+        else -- Air Jump
+            local fall = ply:GetVelocity()
+            print("Air Jump")
+            if fall.z < 0 then
+                ply:SetVelocity(Vector(0, 0, (fall.z - (fall.z * 2) + 300)))
+            else
+                ply:SetVelocity(Vector(0, 0, 300))
+            end
         end
     end
 end)
