0c1eb7dd9a9975d03bbb51cb260ca00b93e983ac
LordBohne2 <work.qj684@aleeas.com>
2023-01-30 13:42:53 +0100

    MidAirJump first version

--- lua/autorun/sh_skywalk.lua
+++ lua/autorun/sh_skywalk.lua
@@ -1,1 +1,13 @@
 print("Skywalk...")
+
+hook.Add("KeyPress", "MidAirJump", function(ply, key)
+    if key == IN_JUMP and ply:OnGround() == false then
+        if key == IN_SPEED then
+            local angle = ply:EyeAngles()
+            local direction = angle:Forward() * 250
+            ply:SetVelocity(ply:GetVelocity() + Vector(direction.x, direction.y, 250))
+        else
+            ply:SetVelocity(Vector(0, 0, 300))
+        end
+    end
+end)
