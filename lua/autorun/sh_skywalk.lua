be7b1fae78d5cf2dd2a8fec586de5758b171533d
LordBohne2 <work.qj684@aleeas.com>
2023-02-01 22:13:46 +0100

    Improvements

--- lua/autorun/sh_skywalk.lua
+++ lua/autorun/sh_skywalk.lua
@@ -1,65 +1,71 @@
 print("Skywalk...")
 
-local skywalk_speed = 600
+local skywalk_base_speed = 600
 local skywalk_max_speed = 1000
+local skywalk_sprint = 300
 local allow_skywalk = true
 local allow_sound = true 
-local allow_particle = true
+local allow_particle = false
 local skywalk_sound_path = "addons/skywalk/sound/skywalk.wav"
 local ParticleEmitter = ParticleEmitter
 
 hook.Add("KeyPress", "MidAirJump", function(ply, key)
+    -- Skywalk
     if allow_skywalk then
         if key == IN_JUMP and ply:OnGround() == false then
-            if true then -- Direction Jump
-                local angle = ply:EyeAngles()
-                local ply_Velocity = ply:GetVelocity() -- Get Player Velocity 
 
-                print("Angle X Y: " .. angle.x .. angle.y)
-                print("Player Velocity X Y Z: " .. ply_Velocity.x .. ply_Velocity.y .. ply_Velocity.z)
+            local skywalk_speed = skywalk_base_speed
+            local angle = ply:EyeAngles()
+            local ply_Velocity = ply:GetVelocity() -- Get Player Velocity
+            print("Angle X Y: " .. angle.x .. angle.y)
+            print("Player Velocity X Y Z: " .. ply_Velocity.x .. ply_Velocity.y .. ply_Velocity.z)
 
+            -- Get Extra Speed
+            if ply:KeyDown(IN_SPEED) then
+                skywalk_speed = skywalk_speed + skywalk_sprint
+
+                if angle.x < -80 then -- Fast Up
+                    ply:SetVelocity(Vector(0, 0, 1000))
+                    return
+                end
+                if angle.x > 80 then -- Fast Down
+                    ply:SetVelocity(Vector(0, 0, -500))
+                    return
+                end
+            end
+
+            -- Direction Jump / Forward
+            if ply:KeyDown(IN_FORWARD) then
                 local player_speed = math.sqrt(ply_Velocity.x^2 + ply_Velocity.y^2)
-
                 print("Player Speed: " .. player_speed)
 
-                local speed = skywalk_speed + player_speed
+                local speed = skywalk_base_speed + player_speed
 
                 -- Limit Speed
                 if speed > skywalk_max_speed then
                     speed = skywalk_max_speed
                 end
 
-                print("Speed: " .. speed)
-
                 local direction = angle:Forward() * speed
 
                 local x = direction.x - ply_Velocity.x
                 local y = direction.y - ply_Velocity.y
-                local z = direction.z - ply_Velocity.z
-
-                if angle.x > -20 then -- Neutral Jump
-                    z = z + 300
-                end
-                if angle.x < -80 then -- Fast Up
-                    z = z + 600
-                end
-                if angle.x > 80 then -- Fast Down
-                    z = ply_Velocity.z - 300
-                end
+                local z = direction.z - ply_Velocity.z + 300
 
                 ply:SetVelocity(Vector(x, y, z))
                 print("Direction Jump")
-
+                
             else -- Air Jump
                 local fall = ply:GetVelocity()
-                print("Air Jump")
                 if fall.z < 0 then
-                    ply:SetVelocity(Vector(0, 0, (fall.z - (fall.z * 2) + skywalk_speed)))
+                    ply:SetVelocity(Vector(0, 0, (fall.z - (fall.z * 2) + 300)))
                 else
-                    ply:SetVelocity(Vector(0, 0, skywalk_speed))
+                    ply:SetVelocity(Vector(0, 0, 300))
                 end
+                print("Air Jump")
             end
             
+            -- Particle
             if allow_particle then
                 local emitter = ParticleEmitter(ply:GetPos())
                 for i = 1, 100 do
@@ -75,6 +81,8 @@ hook.Add("KeyPress", "MidAirJump", function(ply, key)
                     particle:SetColor(255, 255, 255)
                 end
             end
+
+            -- Sound
             if allow_sound then
                 sound.Play(skywalk_sound_path, ply:GetPos(), 75, 100, 1)
             end
@@ -90,7 +98,7 @@ hook.Add("PopulateToolMenu", "CustomMenuSettings", function()
     spawnmenu.AddToolMenuOption("Utilities", "Skywalk", "Skywalk_Menu", "#Skywalk_Menu", "", "", function(panel)
         panel:ClearControls()
         panel:CheckBox("Enable Skywalk", "sv_allow_skywalk")
-        panel:NumSlider("Skywalk Speed", "sv_skywalk_speed", 100, skywalk_max_speed, nil)
+        panel:NumSlider("Skywalk Speed", "sv_skywalk_base_speed", 100, skywalk_max_speed, nil)
         panel:CheckBox("Enable Sound", "sv_allow_skywalk_sound")
     end)
 end)
